f7800863ba2f34685b02ca066ee04144553c76fe
khushi1825 <iamkhushi5621@gmail.com>
2026-01-19 15:22:23 +0530

    final commit

--- src/main/java/com/example/day_2_part_2/controller/Home.java
+++ src/main/java/com/example/day_2_part_2/controller/Home.java
@@ -1,5 +1,5 @@
 package com.example.day_2_part_2.controller;
-
+import java.util.*;
 import com.example.day_2_part_2.model.StudentModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Home {
     @GetMapping("about")
-    public StudentModel getStudent(){
-        StudentModel student=new StudentModel(1,"Khushi","khushi@gmail.com");
-        return student;
+    public List<StudentModel> getStudent(){
+        List<StudentModel> students=new ArrayList<>();
+        students.add(new StudentModel(1, "Khushi", "khushi@gmail.com"));
+        students.add(new StudentModel(2, "Anantika", "anantika@gmail.com"));
+        students.add(new StudentModel(3, "Ananya", "ananya@gmail.com"));
+        students.add(new StudentModel(4, "Divya", "divya@gmail.com"));
+        students.add(new StudentModel(5, "Shanya", "shanya@gmail.com"));
+
+        return students;
+
     }
 }
